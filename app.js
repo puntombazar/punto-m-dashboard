1f2b37b3cdf0f9f9fd8fefdd5032897698de6aa3
puntombazar <puntombazar@gmail.com>
2026-09-03 09:43:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzMLr3-9GUyKbScWlDyF_21X2lumlPl9OZqJ2WPEd8XOPNCXgComIW_COId4Yopll57wQ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyXKwmizQYKy6vRrG2jyc89PEnxIvwx7Wl4u4JpVALB5Z6cdwYqcZsXsfap9ZzZY_dR1A/exec',
   TABS: {
     ESTRATEGIA: '01_ESTRATEGIA_MENSUAL',
     PRODUCTOS:  '02_PRODUCTOS',
